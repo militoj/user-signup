00fa83cc9795ec83426b16a2dfef4532ca9aa968
militoj <militoj@gmail.com>
2017-02-02 17:32:26 -0600

    going to start messing with MainHandler def get buildpage in order to pass a bunch of error arguments

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 import re
 
 
-def build_page(username_entered,email_entered):
+def build_page(username_entered,email_entered,username_error_element,password_error_element,verify_error_element,email_error_element):
     header = "<h1>Signup</h1>"
 
     username_label = "<label>Username</label>"
@@ -33,46 +33,7 @@ def build_page(username_entered,email_entered):
     email_label = "<label>Email (optional)</label>"
     email_input = "<input type='text' name='email' value='{0}'/>".format(email_entered)
 
-    username_error = self.request.get("username_error")
-    password_error = self.request.get("password_error")
-    verify_error = self.request.get("verify_error")
-    email_error = self.request.get("email_error")
 
-    if username_error:
-        username_error_element = (
-            "<p class='error'>" +
-            cgi.escape(username_error, quote=True) +
-            "</p>"
-        )
-    else:
-        username_error_element  = ""
-
-    if password_error:
-        password_error_element = (
-            "<p class='error'>" +
-            cgi.escape(password_error, quote=True) +
-            "</p>"
-        )
-    else:
-        password_error_element  = ""
-
-    if verify_error:
-        verify_error_element = (
-            "<p class='error'>" +
-            cgi.escape(verify_error, quote=True) +
-            "</p>"
-        )
-    else:
-        verify_error_element = ""
-
-    if email_error:
-        email_error_element = (
-            "<p class='error'>" +
-            cgi.escape(email_error, quote=True) +
-            "</p>"
-        )
-    else:
-        email_error_element  = ""
 
 
     submit = "<input type='submit'/>"
@@ -128,26 +89,76 @@ class MainHandler(webapp2.RequestHandler):
         verify_pass = False
         email_pass = False
 
+
+        username_error = ''
+        password_error = ''
+        verify_error = ''
+        email_error = ''
+
+        username_error_element = 'BLAH '
+        password_error_element = 'BLAH '
+        verify_error_element = 'BLAH '
+        email_error_element = 'BLAH '
+
         if username_check(username):
             username_pass = True
         else:
-            error = "Please enter a valid username"
-
-            # redirect to homepage, and include error as a query parameter in the URL
-            self.redirect("/?error=" + error)
+            username_error = "Please enter a valid username"
 
         if password_check(password):
             password_pass = True
+        else:
+            password_error = "Please enter a password"
 
         if verify_check(password,verify):
             verify_pass = True
+        else:
+            verify_error = "Your passwords do not match"
 
         if email_check(email):
             email_pass = True
+        else:
+            email_error = "Please enter a valid email address"
 
 
 
-        content = build_page(username,email)
+        # if username_error:
+        #     username_error_element = (
+        #         "<p class='error'>" +
+        #         cgi.escape(username_error, quote=True) +
+        #         "</p>"
+        #     )
+        # else:
+        #     username_error_element  = " "
+        #
+        # if password_error:
+        #     password_error_element = (
+        #         "<p class='error'>" +
+        #         cgi.escape(password_error, quote=True) +
+        #         "</p>"
+        #     )
+        # else:
+        #     password_error_element  = " "
+        #
+        # if verify_error:
+        #     verify_error_element = (
+        #         "<p class='error'>" +
+        #         cgi.escape(verify_error, quote=True) +
+        #         "</p>"
+        #     )
+        # else:
+        #     verify_error_element = " "
+        #
+        # if email_error:
+        #     email_error_element = (
+        #         "<p class='error'>" +
+        #         cgi.escape(email_error, quote=True) +
+        #         "</p>"
+        #     )
+        # else:
+        #     email_error_element  = " "
+
+        content = build_page(username,email,username_error_element,password_error_element,verify_error_element,email_error_element)
         self.response.write(content)
 
 
